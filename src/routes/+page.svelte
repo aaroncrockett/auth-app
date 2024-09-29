a4d68f88cec752e2db6cd31c26bee7a357a9e579
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-09-28 19:32:56 -0700

    shadcn setup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+</script>
+
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Auth testing app" />
 </svelte:head>
 
 <section>
-	<h1 class="text-red-500 text-6xl">My Auth App</h1>
+	<h1 class="text-6xl text-red-500">My Auth App</h1>
+	<Button>example</Button>
 </section>
