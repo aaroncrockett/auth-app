90c6b7234a01a503ec74d703a1195d82e4f6cd5b
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-09-28 19:20:24 -0700

    add tailwind to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,16 +10,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
+	<h1 class="text-red-500 text-6xl">My Auth App</h1>
 
 	<h2>
 		try editing <strong>src/routes/+page.svelte</strong>
