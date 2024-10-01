87db431ede753188b21e2879f287638688c44f03
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-10-01 13:15:31 -0700

    auth wip

---
+++ src/routes/auth/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+</script>
+
+<form method="POST" action="?/login" class="w-1/2 mx-auto space-y-2">
+	<Input placeholder="email" name="email" type="email" />
+	<Input placeholder="password" name="password" type="password" />
+	<Button formaction="?/signup">Sign up</Button>
+</form>
