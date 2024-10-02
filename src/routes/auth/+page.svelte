7bb58c054723e379de6d7ba6fdabd2731c588623
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-10-02 11:01:10 -0700

    login types

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,12 +1,31 @@
-<script>
+<script lang="ts">
 	import Form from './EmailSignupForm.svelte';
+
+	import { Button } from '$lib/components/ui/button';
+
 	export let data;
 
 	let formType = 'magic';
+
+	let formTypes = [
+		{ id: 'magic', text: 'Magic Link' },
+		{ id: 'instagram', text: 'Instagram' },
+		{ id: 'google', text: 'Google' },
+		{ id: 'facebook', text: 'Facebook' }
+	];
+
+	function updateFormType(type: string) {
+		formType = type;
+	}
 </script>
 
-{#if formType === 'password'}
-	<Form data={data.form} />
-{:else if formType === 'magic'}
-	<p>magic link</p>
-{/if}
+<p>sign in method:</p>
+<ul>
+	{#each formTypes as type}
+		<li class="p-1">
+			<Button variant="secondary" class="w-full" on:click={() => updateFormType(type.id)}>
+				{type.text}
+			</Button>
+		</li>
+	{/each}
+</ul>
